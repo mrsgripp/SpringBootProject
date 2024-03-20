d6b714c1e2a6b5ee72c287638e208518c743663c
wow-latefordinner <wow.latefordinner@gmail.com>
2024-03-20 17:29:45 -0400

    updates to seller functions

---
+++ src/main/java/org/example/exception/SellerNameExistsException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class SellerNameExistsException extends Exception{
+    public SellerNameExistsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/example/repository/SellerRepository.java
+++ src/main/java/org/example/repository/SellerRepository.java
@@ -3,6 +3,9 @@ package org.example.repository;
 import org.example.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+    List<Seller> findByName(String name);
 
 }
